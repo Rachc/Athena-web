33c2806cd0f48d671f43adb85a9dc17bea530ced
Rachel <rachelcurioso@gmail.com>
2017-08-25 13:58:56 -0300

    Parte de landing relativa a cadastro
    vai ser preciso criarmos uma tabela de cadastro antes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 function escondeDiv() {
 	var display = document.getElementById('listaProjetos').style.display;
-	if(display == "none")
-            document.getElementById('listaProjetos').style.display = 'block';
-        else
-            document.getElementById('listaProjetos').style.display = 'none';
+	if(display == "none"){
+    document.getElementById('listaProjetos').style.display = 'block';
+	}else{
+    document.getElementById('listaProjetos').style.display = 'none';
+	}
 }
