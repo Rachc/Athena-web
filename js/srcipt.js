2e8b6247920bea921ae7bfbf4587c92ed1c08622
Jane <30847698+janeferreira@users.noreply.github.com>
2017-08-25 13:18:48 -0300

    Create srcipt.js

---
+++ js/srcipt.js
@@ -0,0 +1,7 @@
+function escondeDiv() {
+	var display = document.getElementById('listaProjetos').style.display;
+	if(display == "none")
+            document.getElementById('listaProjetos').style.display = 'block';
+        else
+            document.getElementById('listaProjetos').style.display = 'none';
+}
