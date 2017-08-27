77c099cb535731690057acaa69d390fe801d97cf
luiszilotti <luis_lol@outlook.com.br>
2017-08-27 16:24:25 -0300

    Revert "Add files via upload"
    This reverts commit 63c14406ef6ad113f9df77c3038e03fc6db42bca.

--- script.js
+++
@@ -1,8 +0,0 @@
-function escondeDiv() {
-	var display = document.getElementById('listaProjetos').style.display;
-	if(display == "none"){
-    document.getElementById('listaProjetos').style.display = 'block';
-	}else{
-    document.getElementById('listaProjetos').style.display = 'none';
-	}
-}
