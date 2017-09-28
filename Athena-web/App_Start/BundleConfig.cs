f5ac752c146a231ef752f65eb9aa01ef0d866fff
Rachel <rachelcurioso@gmail.com>
2017-09-28 09:54:27 -0300

    Criado o layout base e os controllers para projeto

--- Athena-web/App_Start/BundleConfig.cs
+++ Athena-web/App_Start/BundleConfig.cs
@@ -24,8 +24,9 @@ namespace Athena_web
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/css/bootstrap.css",
+                      "~/Content/css/site.css",
+                      "~/Content/css/main.css"));
         }
     }
 }
