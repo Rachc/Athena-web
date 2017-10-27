886821049c9fab8c021e41fd7ce34a356835dd0f
Rachel <rachelcurioso@gmail.com>
2017-10-27 19:25:52 -0200

    Menu do usuário
    Menu do usuário. Feito apenas na página InterfaceUsuario.html

--- Referencia html/js/script.js
+++ Referencia html/js/script.js
@@ -1,8 +1,17 @@
 function escondeDiv() {
 	var display = document.getElementById('listaProjetos').style.display;
-	if(display == "none"){
+	if(display === "none"){
     document.getElementById('listaProjetos').style.display = 'block';
 	}else{
     document.getElementById('listaProjetos').style.display = 'none';
+	}
+}
+
+function menuUsuario() {
+	var display = document.getElementById('menuUsuario').style.display;
+	if(display == "none"){
+    document.getElementById('menuUsuario').style.display = 'block';
+	}else{
+    document.getElementById('menuUsuario').style.display = 'none';
 	}
 }
